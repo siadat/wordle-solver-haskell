68a4f3a93544104d5bb452919786b93aa0c4dfb4
Sina Siadat <siadat@gmail.com>
2022-01-24 00:27:45 +0000

    implement simple guesser
    simple guesser ignores the score of previous history items and goes
    through all the words to find the perfect match.

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,106 @@
 module Lib
-    ( someFunc
+    ( startGame
+    , MasterType(..)
+    , GuessType(..)
+    , JudgeType(..)
+    , judgeWord
+    , simpleGuesser
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import qualified Data.Char as DataChar
+import qualified System.IO as SystemIO
+import qualified Data.Map as DataMap
+import qualified Data.List as DataList
+
+type WordList = [String]
+
+startGame :: IO ()
+startGame = do
+    putStr $ "Enter a new challenge: "
+    SystemIO.hFlush SystemIO.stdout
+    allWords <- readFile "/usr/share/dict/words"
+    let filteredWords = filterWords (lines allWords)
+    master <- getLine
+    let guess = solveWordle (simpleGuesser filteredWords) (judgeWord (Master master))
+    case guess of
+      Just (Guess g) -> putStrLn $ "Found: " ++ g
+      Nothing -> putStrLn $ "No match found"
+    putStrLn $ "You entered: " ++ master
+    putStrLn $ "GAME OVER"
+
+data MasterType = Master String
+data GuessType  = Guess String deriving (Show)
+data JudgeType  = Judge String deriving (Eq, Show)
+
+type History = DataMap.Map GuessType JudgeType
+type Judger  = GuessType -> JudgeType
+type Guesser = History -> Maybe GuessType
+
+instance Eq GuessType where
+  (Guess a) == (Guess b) = a == b
+
+instance Ord GuessType where
+  compare (Guess a) (Guess b) = compare a b
+
+perfectMatch :: JudgeType
+perfectMatch = Judge "22222"
+
+--
+-- solver
+--
+solveWordle :: Guesser -> Judger -> Maybe GuessType
+solveWordle guesser judger = solveWordleRec DataMap.empty guesser judger
+
+solveWordleRec :: History -> Guesser -> Judger -> Maybe GuessType
+solveWordleRec history guesser judger =
+  let guess = guesser history
+  in case guess of
+    Just g -> let j = judger g
+                  h = (DataMap.insert g j history)
+              in solveWordleRec h guesser judger
+    Nothing -> Nothing
+
+simpleGuesser :: WordList -> History -> Maybe GuessType
+simpleGuesser words history =
+  let gs = filter (\w -> not $ DataMap.member (Guess w) history) words
+  in case gs of
+    g:_ -> Just (Guess g)
+    [] -> Nothing
+
+toLowerStr :: String -> String
+toLowerStr = map DataChar.toLower
+
+filterWords :: WordList -> WordList
+filterWords ws = map toLowerStr $ filter (\x -> 5 == length x) ws
+
+
+--
+-- judger
+--
+type CharFreq = DataMap.Map Char Int
+
+judgeWord :: MasterType -> GuessType -> JudgeType
+judgeWord master@(Master m) guess = judgeWordRec master guess (stringCharFreq m) ""
+
+judgeWordRec :: MasterType -> GuessType -> CharFreq -> [Char] -> JudgeType
+judgeWordRec (Master (m:ms)) (Guess (g:gs)) remaining acc =
+  if (DataMap.member g remaining) then
+    if g == m then
+      judgeWordRec (Master ms) (Guess gs) (decrCharFreq g remaining) ('2':acc)
+    else                                 
+      judgeWordRec (Master ms) (Guess gs) (decrCharFreq g remaining) ('1':acc)
+  else                                   
+    judgeWordRec (Master ms) (Guess gs) (decrCharFreq g remaining) ('0':acc)
+judgeWordRec _ _ _ acc = (Judge $ reverse acc)
+
+decrCharFreq :: Char -> CharFreq -> CharFreq
+decrCharFreq c freqs =
+  let updated = DataMap.insertWith (+) c (-1) $ freqs
+  in DataMap.filter (\v -> v > 0) updated
+
+stringCharFreq :: String -> CharFreq
+stringCharFreq word = DataMap.fromList
+                    $ Prelude.map (\x->((head x), (length x)))
+                    $ DataList.group
+                    $ DataList.sort word
+
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,18 @@
+import Test.Hspec
+import Lib
+
+import qualified Data.Map as DataMap
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+    describe "judger" $ do
+        it "should judge word" $ do
+            judgeWord (Master "abcde") (Guess "abcde") `shouldBe` (Judge "22222")
+            judgeWord (Master "abcde") (Guess "abxyc") `shouldBe` (Judge "22001")
+            judgeWord (Master "a") (Guess "abcde") `shouldBe` (Judge "2")
+            judgeWord (Master "abcde") (Guess "a") `shouldBe` (Judge "2")
+        it "should guess a word" $ do
+            simpleGuesser ["abcde", "abcxy"] DataMap.empty `shouldBe` Just (Guess "abcde")
+            simpleGuesser ["abcde", "abcxy"] (DataMap.fromList [(Guess "abcde", Judge "00000")]) `shouldBe` Just (Guess "abcxy")
+            simpleGuesser ["abcde", "abcxy"] (DataMap.fromList [(Guess "abcxy", Judge "00000")]) `shouldBe` Just (Guess "abcde")
+            simpleGuesser ["abcde", "abcxy"] (DataMap.fromList [(Guess "abcde", Judge "00000"), (Guess "abcxy", Judge "00000")]) `shouldBe` Nothing
