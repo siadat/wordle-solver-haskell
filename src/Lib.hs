c61e7377ab5ed7a8c210a1b33d097cb1e584ab06
Sina Siadat <siadat@gmail.com>
2022-01-29 23:40:30 +0000

    rename judge to colors

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,15 +2,15 @@ module Lib
     ( startGame
     , MasterType(..)
     , GuessType(..)
-    , JudgeType(..)
+    , ColorsType(..)
     , judgeWord
     , simpleGuesser
     , betterGuesser
     , betterGuesserAll
     , checkMask
     , maskFunc
-    , filterGuessByJudge
-    , stringCharFreq
+    , filterGuessByColor
+    , countCharFreq
     , matchWord
     , checkFreqs
     , MaskType
@@ -26,14 +26,14 @@ type WordList = [String]
 
 newtype MasterType = Master String
 newtype GuessType  = Guess String deriving (Show)
-newtype JudgeType  = Judge String deriving (Eq, Show)
+newtype ColorsType  = Colors String deriving (Eq, Show)
 newtype ScoredGuessType = ScoredGuess (GuessType, Int)
 
-type History = DataMap.Map GuessType JudgeType
-type HistoryList = [(GuessType, JudgeType)]
-type Judger  = GuessType -> JudgeType
+type History = DataMap.Map GuessType ColorsType
+type HistoryList = [(GuessType, ColorsType)]
+type Judger  = GuessType -> ColorsType
 type Guesser = History -> Maybe GuessType
-type Candidate = String
+type CandidateType = String
 
 instance Eq GuessType where
   (Guess a) == (Guess b) = a == b
@@ -41,8 +41,8 @@ instance Eq GuessType where
 instance Ord GuessType where
   compare (Guess a) (Guess b) = compare a b
 
-perfectMatch :: JudgeType
-perfectMatch = Judge "22222"
+perfectMatch :: ColorsType
+perfectMatch = Colors "22222"
 
 startGame :: IO ()
 startGame = do
@@ -52,7 +52,7 @@ startGame = do
 
   gen <- SystemRandom.newStdGen
   let zerosAndOnes = SystemRandom.randomRs (0, 10000) gen :: [Int]
-  let shuffledWords = map (\ (_, x) -> x) $ DataList.sortBy (\(i, _) (j, _) -> compare (zerosAndOnes !! i) (zerosAndOnes !! j)) $ zip [0..] filteredWords
+  let shuffledWords = map (\ (_, x) -> x) $ DataList.sortBy (\(i, _) (c, _) -> compare (zerosAndOnes !! i) (zerosAndOnes !! c)) $ zip [0..] filteredWords
   putStrLn "Guide:"
   putStrLn "0     = black"
   putStrLn "1     = yellow"
@@ -63,8 +63,8 @@ startGame = do
   playTurn shuffledWords history
   putStrLn "Bye"
 
-showHistoryItem :: (GuessType, JudgeType) -> String
-showHistoryItem ((Guess g), (Judge j)) = "History | guess: " ++ g ++ " " ++ j
+showHistoryItem :: (GuessType, ColorsType) -> String
+showHistoryItem ((Guess g), (Colors c)) = "History | guess: " ++ g ++ " " ++ c
 
 playTurn :: WordList -> History -> IO ()
 playTurn filteredWords history = do
@@ -78,15 +78,15 @@ playTurn filteredWords history = do
   putStr "Enter colors here: "
   SystemIO.hFlush SystemIO.stdout
 
-  judge <- getLine
+  colors <- getLine
   case newGuess of
     Nothing ->
       putStrLn "Nothing found"
     Just g ->
-      if (Judge judge) == perfectMatch then
+      if (Colors colors) == perfectMatch then
         putStrLn "Found!"
       else
-        let newHistory = DataMap.insert g (Judge judge) history
+        let newHistory = DataMap.insert g (Colors colors) history
          in playTurn filteredWords newHistory
 
 filterWords :: WordList -> WordList
@@ -100,8 +100,8 @@ simpleScorer words history =
   let isInHistory w = DataMap.member (Guess w) history
    in map (f isInHistory) $ DataMap.toList history
     where
-        f :: (String -> Bool) -> (GuessType, JudgeType) -> ScoredGuessType
-        f isIncludedFunc (Guess g, Judge _)
+        f :: (String -> Bool) -> (GuessType, ColorsType) -> ScoredGuessType
+        f isIncludedFunc (Guess g, Colors _)
             | isIncludedFunc g == True = ScoredGuess ((Guess g), 0)
             | otherwise                = ScoredGuess ((Guess g), 1)
 
@@ -124,18 +124,19 @@ betterGuesserAll words history =
   map (\w -> (Guess w)) $ filter (\w -> checkAgainstHistory (DataMap.toList history) w) words
 
 checkAgainstHistory :: HistoryList -> String -> Bool
-checkAgainstHistory ((g, j):hs) candida = matchWord g j candida && checkAgainstHistory hs candida
-checkAgainstHistory [] candida = True
+checkAgainstHistory ((g, c):hs) candidate = matchWord g c candidate && checkAgainstHistory hs candidate
+checkAgainstHistory [] candidate = True
 
-matchWord :: GuessType -> JudgeType -> Candidate -> Bool
-matchWord guess@(Guess gs) judge@(Judge js) candida =
-  let eqs = maskFunc guess judge (== '2')
-      nqs = maskFunc guess judge (/= '2')
-      condGreens = checkMask eqs candida (==)
-      condOthers = checkMask nqs candida (/=)
-      cFreqs = stringCharFreq (filterGuessByJudge (/= '2') (Guess candida) judge)
-      yFreqs = stringCharFreq (filterGuessByJudge (=='1') guess judge)
-      bFreqs = stringCharFreq (filterGuessByJudge (=='0') guess judge)
+
+matchWord :: GuessType -> ColorsType -> CandidateType -> Bool
+matchWord guess@(Guess gs) colors@(Colors cs) candidate =
+  let eqs = maskFunc guess colors (== '2')
+      nqs = maskFunc guess colors (/= '2')
+      condGreens = checkMask eqs candidate (==)
+      condOthers = checkMask nqs candidate (/=)
+      cFreqs = countCharFreq (filterGuessByColor (/= '2') (Guess candidate) colors)
+      yFreqs = countCharFreq (filterGuessByColor (=='1') guess colors)
+      bFreqs = countCharFreq (filterGuessByColor (=='0') guess colors)
       condFreqs = checkFreqs cFreqs yFreqs bFreqs gs
    in
       condGreens && condOthers && condFreqs
@@ -160,11 +161,11 @@ checkFreqs candidaFreqs guessYellowFreqs guessBlackFreqs (g:gs) =
   in ok && checkFreqs candidaFreqs guessYellowFreqs guessBlackFreqs gs
 checkFreqs _ _ _ [] = True
 
-filterGuessByJudge :: (Char -> Bool) -> GuessType -> JudgeType -> String
-filterGuessByJudge f (Guess (g:gs)) (Judge (j:js))
-  | (f j) = g:(filterGuessByJudge f (Guess gs) (Judge js))
-  | otherwise = (filterGuessByJudge f (Guess gs) (Judge js))
-filterGuessByJudge f (Guess _) (Judge _) = []
+filterGuessByColor :: (Char -> Bool) -> GuessType -> ColorsType -> String
+filterGuessByColor f (Guess (g:gs)) (Colors (c:cs))
+  | (f c) = g:(filterGuessByColor f (Guess gs) (Colors cs))
+  | otherwise = (filterGuessByColor f (Guess gs) (Colors cs))
+filterGuessByColor f (Guess _) (Colors _) = []
 
 type MaskType = String
 checkMask :: MaskType -> String -> (Char -> Char -> Bool) -> Bool
@@ -173,19 +174,19 @@ checkMask (g:ms) (w:ws) f = (f g w) && checkMask ms ws f
 checkMask [] [] _ = True
 checkMask _ _ _ = False
 
-maskFunc :: GuessType -> JudgeType -> (Char -> Bool) -> MaskType
-maskFunc (Guess (g:gs)) (Judge (j:js)) f =
-  let c = (if (f j) then g else '_')
-      l = maskFunc (Guess gs) (Judge js) f
-   in (c:l)
-maskFunc (Guess _) (Judge _) f = ""
+maskFunc :: GuessType -> ColorsType -> (Char -> Bool) -> MaskType
+maskFunc (Guess (g:gs)) (Colors (c:cs)) f =
+  let ch = (if (f c) then g else '_')
+      l = maskFunc (Guess gs) (Colors cs) f
+   in (ch:l)
+maskFunc (Guess _) (Colors _) f = ""
 
 type CharFreq = DataMap.Map Char Int
 
-judgeWord :: MasterType -> GuessType -> JudgeType
-judgeWord master@(Master m) guess = judgeWordRec master guess (stringCharFreq m) ""
+judgeWord :: MasterType -> GuessType -> ColorsType
+judgeWord master@(Master m) guess = judgeWordRec master guess (countCharFreq m) ""
   where
-    judgeWordRec :: MasterType -> GuessType -> CharFreq -> [Char] -> JudgeType
+    judgeWordRec :: MasterType -> GuessType -> CharFreq -> [Char] -> ColorsType
     judgeWordRec (Master (m:ms)) (Guess (g:gs)) remaining acc =
       let prependAndContinue c = judgeWordRec (Master ms) (Guess gs) (decrCharFreq g remaining) (c:acc)
       in
@@ -196,15 +197,15 @@ judgeWord master@(Master m) guess = judgeWordRec master guess (stringCharFreq m)
             prependAndContinue '1'
         else
           prependAndContinue '0'
-    judgeWordRec _ _ _ acc = (Judge $ reverse acc)
+    judgeWordRec _ _ _ acc = (Colors $ reverse acc)
 
 decrCharFreq :: Char -> CharFreq -> CharFreq
 decrCharFreq c freqs =
   let updated = DataMap.insertWith (+) c (-1) $ freqs
   in DataMap.filter (> 0) updated
 
-stringCharFreq :: String -> CharFreq
-stringCharFreq word = DataMap.fromList
+countCharFreq :: String -> CharFreq
+countCharFreq word = DataMap.fromList
                     $ Prelude.map (\x->((head x), (length x)))
                     $ DataList.group
                     $ DataList.sort word
--- test/Spec.hs
+++ test/Spec.hs
@@ -7,55 +7,55 @@ main :: IO ()
 main = hspec $ do
     describe "judger" $ do
         it "should judge word" $ do
-            judgeWord (Master "abcde") (Guess "abcde") `shouldBe` (Judge "22222")
-            judgeWord (Master "abcde") (Guess "abxyc") `shouldBe` (Judge "22001")
-            judgeWord (Master "a") (Guess "abcde") `shouldBe` (Judge "2")
-            judgeWord (Master "abcde") (Guess "a") `shouldBe` (Judge "2")
+            judgeWord (Master "abcde") (Guess "abcde") `shouldBe` (Colors "22222")
+            judgeWord (Master "abcde") (Guess "abxyc") `shouldBe` (Colors "22001")
+            judgeWord (Master "a") (Guess "abcde") `shouldBe` (Colors "2")
+            judgeWord (Master "abcde") (Guess "a") `shouldBe` (Colors "2")
         it "should guess a word (brute-force)" $ do
             simpleGuesser ["abcde", "abcxy"] DataMap.empty `shouldBe` Just (Guess "abcde")
-            simpleGuesser ["abcde", "abcxy"] (DataMap.fromList [(Guess "abcde", Judge "00000")]) `shouldBe` Just (Guess "abcxy")
-            simpleGuesser ["abcde", "abcxy"] (DataMap.fromList [(Guess "abcxy", Judge "00000")]) `shouldBe` Just (Guess "abcde")
-            simpleGuesser ["abcde", "abcxy"] (DataMap.fromList [(Guess "abcde", Judge "00000"), (Guess "abcxy", Judge "00000")]) `shouldBe` Nothing
+            simpleGuesser ["abcde", "abcxy"] (DataMap.fromList [(Guess "abcde", Colors "00000")]) `shouldBe` Just (Guess "abcxy")
+            simpleGuesser ["abcde", "abcxy"] (DataMap.fromList [(Guess "abcxy", Colors "00000")]) `shouldBe` Just (Guess "abcde")
+            simpleGuesser ["abcde", "abcxy"] (DataMap.fromList [(Guess "abcde", Colors "00000"), (Guess "abcxy", Colors "00000")]) `shouldBe` Nothing
         it "...stuff..." $ do
-            maskFunc (Guess "abc") (Judge "120") (\x -> x == '2') `shouldBe` "_b_"
-            maskFunc (Guess "abc") (Judge "120") (\x -> x /= '2') `shouldBe` "a_c"
+            maskFunc (Guess "abc") (Colors "120") (\x -> x == '2') `shouldBe` "_b_"
+            maskFunc (Guess "abc") (Colors "120") (\x -> x /= '2') `shouldBe` "a_c"
             checkMask "a__" "abc" (==) `shouldBe` True
             checkMask "b__" "abc" (==) `shouldBe` False
             checkMask "a__" "abc" (/=) `shouldBe` False
             checkMask "b__" "abc" (/=) `shouldBe` True
-            filterGuessByJudge (=='1') (Guess "abc") (Judge "121") `shouldBe` "ac"
-            filterGuessByJudge (=='2') (Guess "abc") (Judge "002") `shouldBe` "c"
-            stringCharFreq "hello" `shouldBe` DataMap.fromList [('h', 1), ('e', 1), ('l', 2), ('o', 1)]
+            filterGuessByColor (=='1') (Guess "abc") (Colors "121") `shouldBe` "ac"
+            filterGuessByColor (=='2') (Guess "abc") (Colors "002") `shouldBe` "c"
+            countCharFreq "hello" `shouldBe` DataMap.fromList [('h', 1), ('e', 1), ('l', 2), ('o', 1)]
 
-            matchWord (Guess "hello") (Judge "22222") "hello" `shouldBe` True
-            matchWord (Guess "abc") (Judge "210") "axb" `shouldBe` True --
-            matchWord (Guess "abc") (Judge "210") "abc" `shouldBe` False
-            matchWord (Guess "bc") (Judge "11") "ab" `shouldBe` False
-            matchWord (Guess "bc") (Judge "11") "cb" `shouldBe` True
+            matchWord (Guess "hello") (Colors "22222") "hello" `shouldBe` True
+            matchWord (Guess "abc") (Colors "210") "axb" `shouldBe` True --
+            matchWord (Guess "abc") (Colors "210") "abc" `shouldBe` False
+            matchWord (Guess "bc") (Colors "11") "ab" `shouldBe` False
+            matchWord (Guess "bc") (Colors "11") "cb" `shouldBe` True
         it "should guess a word (better)" $ do
 
             let twoLetterWithoutRepeat = ["ab", "ba", "ac", "ca", "bc", "cb"]
              in do
                 betterGuesser ["a", "b", "c"] DataMap.empty `shouldBe` Just (Guess "a")
-                betterGuesser twoLetterWithoutRepeat (DataMap.fromList [(Guess "bc", Judge "10")]) `shouldBe` Just (Guess "ab")
-                betterGuesser twoLetterWithoutRepeat (DataMap.fromList [(Guess "bc", Judge "11")]) `shouldBe` Just (Guess "cb")
-                betterGuesser twoLetterWithoutRepeat (DataMap.fromList [(Guess "bc", Judge "01")]) `shouldBe` Just (Guess "ca")
-                betterGuesser twoLetterWithoutRepeat (DataMap.fromList [(Guess "bc", Judge "02")]) `shouldBe` Just (Guess "ac")
-                betterGuesser twoLetterWithoutRepeat (DataMap.fromList [(Guess "bc", Judge "20")]) `shouldBe` Just (Guess "ba")
-                betterGuesser twoLetterWithoutRepeat (DataMap.fromList [(Guess "ab", Judge "00")]) `shouldBe` Nothing
+                betterGuesser twoLetterWithoutRepeat (DataMap.fromList [(Guess "bc", Colors "10")]) `shouldBe` Just (Guess "ab")
+                betterGuesser twoLetterWithoutRepeat (DataMap.fromList [(Guess "bc", Colors "11")]) `shouldBe` Just (Guess "cb")
+                betterGuesser twoLetterWithoutRepeat (DataMap.fromList [(Guess "bc", Colors "01")]) `shouldBe` Just (Guess "ca")
+                betterGuesser twoLetterWithoutRepeat (DataMap.fromList [(Guess "bc", Colors "02")]) `shouldBe` Just (Guess "ac")
+                betterGuesser twoLetterWithoutRepeat (DataMap.fromList [(Guess "bc", Colors "20")]) `shouldBe` Just (Guess "ba")
+                betterGuesser twoLetterWithoutRepeat (DataMap.fromList [(Guess "ab", Colors "00")]) `shouldBe` Nothing
                 
             let threeLetterWords = [[x, y, z] | x <- ['a'..'c'], y <- ['a'..'c'], z <- ['a'..'c']] --- "aaa", "aab", ...
              in do
-                betterGuesser threeLetterWords (DataMap.fromList [(Guess "aaa", Judge "000")]) `shouldBe` Just (Guess "bbb")
-                betterGuesser threeLetterWords (DataMap.fromList [(Guess "aaa", Judge "200")]) `shouldBe` Just (Guess "abb")
-                betterGuesser threeLetterWords (DataMap.fromList [(Guess "aaa", Judge "220")]) `shouldBe` Just (Guess "aab")
-                betterGuesser threeLetterWords (DataMap.fromList [(Guess "aab", Judge "000")]) `shouldBe` Just (Guess "ccc")
-                betterGuesser threeLetterWords (DataMap.fromList [(Guess "aab", Judge "100")]) `shouldBe` Just (Guess "cca")
-                betterGuesser threeLetterWords (DataMap.fromList [(Guess "aab", Judge "120")]) `shouldBe` Just (Guess "caa")
-                betterGuesser threeLetterWords (DataMap.fromList [(Guess "aab", Judge "001")]) `shouldBe` Just (Guess "bbc")
-                betterGuesser threeLetterWords (DataMap.fromList [(Guess "aab", Judge "101")]) `shouldBe` Just (Guess "bba")
-                betterGuesser threeLetterWords (DataMap.fromList [(Guess "aab", Judge "200")]) `shouldBe` Just (Guess "acc")
-                betterGuesser threeLetterWords (DataMap.fromList [(Guess "aab", Judge "201")]) `shouldBe` Just (Guess "abc")
-                betterGuesser threeLetterWords (DataMap.fromList [(Guess "aab", Judge "210")]) `shouldBe` Just (Guess "aca")
-                betterGuesser threeLetterWords (DataMap.fromList [(Guess "aab", Judge "211")]) `shouldBe` Just (Guess "aba")
-                betterGuesser threeLetterWords (DataMap.fromList [(Guess "aaa", Judge "200"), (Guess "abb", Judge "220")]) `shouldBe` Just (Guess "abc")
+                betterGuesser threeLetterWords (DataMap.fromList [(Guess "aaa", Colors "000")]) `shouldBe` Just (Guess "bbb")
+                betterGuesser threeLetterWords (DataMap.fromList [(Guess "aaa", Colors "200")]) `shouldBe` Just (Guess "abb")
+                betterGuesser threeLetterWords (DataMap.fromList [(Guess "aaa", Colors "220")]) `shouldBe` Just (Guess "aab")
+                betterGuesser threeLetterWords (DataMap.fromList [(Guess "aab", Colors "000")]) `shouldBe` Just (Guess "ccc")
+                betterGuesser threeLetterWords (DataMap.fromList [(Guess "aab", Colors "100")]) `shouldBe` Just (Guess "cca")
+                betterGuesser threeLetterWords (DataMap.fromList [(Guess "aab", Colors "120")]) `shouldBe` Just (Guess "caa")
+                betterGuesser threeLetterWords (DataMap.fromList [(Guess "aab", Colors "001")]) `shouldBe` Just (Guess "bbc")
+                betterGuesser threeLetterWords (DataMap.fromList [(Guess "aab", Colors "101")]) `shouldBe` Just (Guess "bba")
+                betterGuesser threeLetterWords (DataMap.fromList [(Guess "aab", Colors "200")]) `shouldBe` Just (Guess "acc")
+                betterGuesser threeLetterWords (DataMap.fromList [(Guess "aab", Colors "201")]) `shouldBe` Just (Guess "abc")
+                betterGuesser threeLetterWords (DataMap.fromList [(Guess "aab", Colors "210")]) `shouldBe` Just (Guess "aca")
+                betterGuesser threeLetterWords (DataMap.fromList [(Guess "aab", Colors "211")]) `shouldBe` Just (Guess "aba")
+                betterGuesser threeLetterWords (DataMap.fromList [(Guess "aaa", Colors "200"), (Guess "abb", Colors "220")]) `shouldBe` Just (Guess "abc")
